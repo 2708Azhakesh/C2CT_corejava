50a334c1b9e5ec274417e98b442a0399fb7a23a2
ram <ram@LAPTOP-MQK1CFS8>
2024-08-02 14:03:56 +0530

    hi

--- akash/src/akash/akash12.java
+++ akash/src/akash/akash12.java
@@ -1,17 +1,20 @@
 package akash;
+//import java.lang.System;
 import java.util.Scanner;
+ 
 public class akash12 {
 
 	public static void main(String[] args)
+	
 	{
-		Scanner akash = new Scanner(System.in);
-		String name = akash.nextLine();
-		int age = akash.nextInt();
-		System.out.println("name"+name);
-		System.out.println("age"+age);
+		Scanner ak = new Scanner(System.in);
+		int a = ak.nextInt();
+	int b = ak.nextInt();
+	
+	System.out.println(a+b);
+	 String c= ak.nextLine();
+	System.out.println(c+"ashok");
 		
-		// TODO Auto-generated method stub
-
 	}
 
 }
