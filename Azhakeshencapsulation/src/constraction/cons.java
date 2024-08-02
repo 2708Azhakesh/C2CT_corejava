31bf7cbd8bd498c7bf136b857df9011e3693f32f
ram <ram@LAPTOP-MQK1CFS8>
2024-08-02 11:21:43 +0530

    azhakesh

---
+++ Azhakeshencapsulation/src/constraction/cons.java
@@ -0,0 +1,16 @@
+package constraction;
+
+public class cons {
+	cons(){
+		System.out.println("akash");
+		
+	}
+	cons(int a){
+		System.out.println("phone:"+a);
+	}
+	
+	cons(float b){
+	System.out.println("presentage:"+b);
+	}
+
+}
---
+++ Azhakeshencapsulation/src/constraction/constmain.java
@@ -0,0 +1,15 @@
+package constraction;
+
+public class constmain {
+
+	public static void main(String[] args) {
+		cons ak = new cons();
+		cons ak1= new cons(253647);
+		cons ak2= new cons(46.4f);
+		
+		
+		
+
+	}
+
+}
